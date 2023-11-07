1286051ecf996e15c76a5276d63771a42835e707
Florian Philippe <florian.philippegm@gmail.com>
2023-11-07 14:57:10 +0100

    Ajustement exercices URL (nettoyage des données code HTTP et encodage)

--- mini-projet/programmes/miniprojet.sh
+++ mini-projet/programmes/miniprojet.sh
@@ -10,8 +10,8 @@ counter=1
 while read -r line
 do
     infos_page=$(curl -L -I $line )
-    code_reponse=$(echo "$infos_page" | egrep "HTTP" | tail -1 |tr -d "\r\d")
-    encodage=$(echo "$infos_page" | egrep -o "charset.+\b")
+    code_reponse=$(echo "$infos_page" | egrep "HTTP" | tail -1 |tr -d "\r\d"|egrep -o "[0-9]{3}")
+    encodage=$(echo "$infos_page" | egrep -o "charset.+\b" | tr -d "charset=") 
     echo -e "${counter}\t${line}\t${code_reponse}\t${encodage}"
     ((counter++))
 
